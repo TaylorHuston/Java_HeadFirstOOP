7c660e097967b1c24e66d85cbfb1890a1f9a9190
Taylor Huston <taylor.a.huston@gmail.com>
2016-05-10 20:21:09 -0700

    Copy in classes from Chapter 2

---
+++ Chapter3/DogDoorSimulation.java
@@ -0,0 +1,18 @@
+package Chapter3;
+
+
+public class DogDoorSimulation {
+    public static void main(String[] args) {
+        DogDoor door = new DogDoor();
+        Remote remote = new Remote(door);
+
+        remote.pressButton();
+
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {
+        }
+
+        remote.pressButton();
+    }
+}
