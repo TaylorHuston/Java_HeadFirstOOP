58f3ac198f980c7fa66400c3684c9394a7a27251
Taylor Huston <taylor.a.huston@gmail.com>
2016-05-10 20:35:17 -0700

    Add BarkRecognizer class

---
+++ Chapter3/BarkRecognizer.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+public class BarkRecognizer {
+
+    private DogDoor door;
+
+    public BarkRecognizer(DogDoor door) {
+        this.door = door;
+    }
+
+    public void recognize(String bark) {
+        System.out.println("Bark Recognizer heard a '" + bark + "'");
+        door.open();
+    }
+}
