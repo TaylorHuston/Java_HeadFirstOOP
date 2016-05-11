20b77379d4ae85346b8e59a99d93a159c8fd00d5
Taylor Huston <taylor.a.huston@gmail.com>
2016-05-10 20:40:59 -0700

    Set door to always automatically close

--- Chapter3/Remote.java
+++ Chapter3/Remote.java
@@ -1,8 +1,5 @@
 package Chapter3;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Remote {
     private DogDoor door;
 
@@ -17,14 +14,6 @@ public class Remote {
         } else {
             door.open();
         }
-
-        final Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            public void run() {
-                door.close();
-                timer.cancel();
-            }
-        }, 5000);
 
     }
 }
